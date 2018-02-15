35ee93e2e8a3e09174c8191c29ed5bb35903dc68
Ruchit Gandhi <gemini.ruchit@gmail.com>
2018-02-14 18:24:01 -0800

    First Draft

---
+++ protractor.conf.js
@@ -0,0 +1,33 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+  seleniumAddress: 'http://seleniumhub:4444/wd/hub',
+  specs: [
+    './e2e/**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome',
+     'chromeOptions': {
+          args: ['no-sandbox',
+            '--disable-web-security', '--headless'],
+        },
+  },
+//  directConnect: true,
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function() {}
+  },
+  onPrepare() {
+    browser.baseUrl = 'http://test:4200/';
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  }
+};
